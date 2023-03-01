bcccddc1c6d29e1e96b0952359150d2f8ce6f534
Alex <y10b@ya.ru>
2023-03-01 12:50:55 +0300

    Initial Commit

---
+++ Father's_kick/CubeView.swift
@@ -0,0 +1,9 @@
+//
+//  CubeView.swift
+//  Father's_kick
+//
+//  Created by Alex on 01.03.2023.
+//
+
+import UIKit
+
---
+++ Father's_kick/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Father's_kick
+//
+//  Created by Alex on 01.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
