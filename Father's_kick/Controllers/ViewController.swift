e1866ef9c225f869c6077ef2bd98fb1a8211e9c1
Alex <y10b@ya.ru>
2023-03-07 21:09:04 +0300

    Task 3

--- Father's_kick/Controllers/ViewController.swift
+++ Father's_kick/Controllers/ViewController.swift
@@ -9,46 +9,89 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private lazy var firstButton = CustomButton().createButton(withTitle: "First Button")
-    private lazy var secondButton = CustomButton().createButton(withTitle: "Second Button")
-    private lazy var thirdButton = CustomButton().createButton(withTitle: "Third")
+    private lazy var cubeView = View()
+    private lazy var transformSlider = UISlider(frame: CGRect(x: 20, y: 250, width: view.frame.width - 40, height: 40))
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .secondarySystemBackground
-        createTargets()
-        makeConstraint()
-        
+        cubeView.frame = .init(x: 20, y: 100, width: 100, height: 100)
+        view.addSubview(cubeView)
+        view.addSubview(transformSlider)
+        transformSlider.minimumValue = 0
+        transformSlider.maximumValue = 1
+        transformSlider.addTarget(self, action: #selector(transformCubeView(slider:event:)), for: .valueChanged)
     }
     
-    private func update() {
-        thirdButton.configurationUpdateHandler = { button in
-            var config = button.configuration
-            config?.baseBackgroundColor = .gray
-            button.configuration = config
+    @objc func transformCubeView(slider: UISlider, event: UIEvent){
+        if let touchEvent = event.allTouches?.first {
+            switch touchEvent.phase {
+            case .began:
+                print("")
+            case .moved:
+                let transform = getTransform(for: slider)
+                UIView.animateKeyframes(withDuration: 0.6, delay: 0.0, options: [.calculationModeLinear], animations: {
+                    UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1.0, animations: {
+                        self.cubeView.transform = transform
+                    })
+                })
+            case .ended:
+                transformSlider.setValue(1, animated: true)
+                let transform = getTransform(for: slider)
+                UIView.animateKeyframes(withDuration: 0.6, delay: 0.0, options: [], animations: {
+                    UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1.0, animations: {
+                        self.cubeView.transform = transform
+                    })
+                })
+            default:
+                break
+            }
         }
     }
     
-    private func createTargets() {
-        thirdButton.addTarget(nil, action: #selector(buttonTapped), for: .touchDown)
+    func getTransform(for slider: UISlider) -> CGAffineTransform {
+        var transform = CGAffineTransform.identity
+        let angle = CGFloat(slider.value) * .pi / 2
+        let width = UIScreen.main.bounds.width
+        let moveRightTransform = CGAffineTransform(translationX: (width - 165) * CGFloat(slider.value), y: 0)
+        let scale = 1 + CGFloat(slider.value) * 0.5
+        let scaleTransform = CGAffineTransform(scaleX: scale, y: scale)
+        transform = CGAffineTransform(rotationAngle: angle)
+        transform = transform.concatenating(scaleTransform)
+        transform = transform.concatenating(moveRightTransform)
+        viewDidLayoutSubviews()
+        return transform
     }
-    
-    @objc func buttonTapped(_ sender: UIButton) {
-        let viewControllerToPresent = UIViewController()
-        viewControllerToPresent.view.backgroundColor = .white
-        self.present(viewControllerToPresent, animated: true)
-    }
-    
-    private func makeConstraint() {
+}
 
-        [firstButton, secondButton, thirdButton].forEach {
-            view.addSubview($0)
+
+class View: UIView {
+    
+    let gradientLayer = CAGradientLayer()
+    
+    init(){
+        super.init(frame: .zero)
+        layer.cornerRadius = 8
+        layer.cornerCurve = .continuous
+        layer.shadowOpacity = 0.3
+        layer.shadowColor = UIColor.blue.cgColor
+        layer.shadowOffset = .init(width: .zero, height: 12)
+        layer.shadowRadius = 12
+        gradientLayer.startPoint = .init(x: 0, y: 0)
+        gradientLayer.endPoint = .init(x: 1, y: 0.5)
+        gradientLayer.colors = [UIColor.systemBlue.cgColor, UIColor.systemRed.cgColor]
+        layer.addSublayer(gradientLayer)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSublayers(of layer: CALayer) {
+        super.layoutSublayers(of: layer)
+        if layer == self.layer {
+            gradientLayer.frame = bounds
+            gradientLayer.cornerRadius = layer.cornerRadius
         }
-        firstButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        firstButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        secondButton.topAnchor.constraint(equalTo: firstButton.bottomAnchor, constant: 8).isActive = true
-        secondButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        thirdButton.topAnchor.constraint(equalTo: secondButton.bottomAnchor, constant: 8).isActive = true
-        thirdButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
 }
